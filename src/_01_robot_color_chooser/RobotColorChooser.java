6661a04f7ab4cc6a972cadca532974a703fc35d7
League Student <league@iMac-24.attlocal.net>
2020-02-08 11:29:29 -0800

    Started Crazy cat lady

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,47 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
 
-        //6. If the user doesn’t enter anything, choose a random color
+		Robot bob = new Robot("batman");
+		// 3. Ask the user what color they would like the robot to draw
+		for (; true;) {
+			String color = JOptionPane
+					.showInputDialog("What color whould you like the robot to draw? (Red Green or Blue)");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (color.equalsIgnoreCase("red")) {
+				bob.setPenColor(255, 0, 0);
+			} else if (color.equalsIgnoreCase("green")) {
+				bob.setPenColor(0, 255, 0);
+			} else if (color.equalsIgnoreCase("blue")) {
+				bob.setPenColor(0, 0, 255);
+			} else {
+				bob.setRandomPenColor();
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+			}
+			// 6. If the user doesn’t enter anything, choose a random color
 
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
+
+			// 4. Set the pen width to 10
+			bob.setPenWidth(10);
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+			bob.penDown();
+			bob.setSpeed(16);
+			for (int i = 0; i < 4; i++) {
+				bob.move(100);
+				bob.turn(90);
+			}
+			bob.penUp();
+		}
 
 	}
 }
