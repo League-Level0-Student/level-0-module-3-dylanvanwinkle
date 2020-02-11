f15e47a363c453a37605b2e000c472e661a6cb5f
League Student <league@WTS14-3.attlocal.net>
2020-02-10 17:01:55 -0800

    Crazy cat lady, High Low Game
    Fun!!

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,19 +5,34 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+		String Cats = JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-
+		int C = Integer.parseInt(Cats);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if (C >= 3) {
+			JOptionPane.showMessageDialog(null, "You are a crazy cat Person.");
+		} else if (C < 3 && C > 0) {
+			playVideo("https://www.youtube.com/watch?v=7cTMbkH-08Y");
 
-		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
-		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+		}else {
+			
+		playVideo("https://www.youtube.com/watch?v=ZJT9CeEhM10");
+			
+		}
 		
 	}
+	// 4. If they have less than 3 cats AND more than 0 cats, call the method below
+	// to show them a cat video
+
+	// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a
+	// Human
+
+	
 
 	static void playVideo(String videoURL) {
 		try {
@@ -29,4 +44,3 @@ public class CrazyCatLady {
 	}
 
 }
-
