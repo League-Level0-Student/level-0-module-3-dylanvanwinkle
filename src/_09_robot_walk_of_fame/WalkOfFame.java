2d5db6088fef51ae30774f069640b174465f3ced
League Student <league@iMac-16.attlocal.net>
2020-03-02 16:59:49 -0800

    99bottelsIP

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -8,22 +8,28 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		Robot a = new Robot();
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-
+		a.setX(40);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+		a.setSpeed(100);
+		a.penDown();
+		for (int i = 0; i < 6; i++) {
+			a.penDown();
+			a.setPenWidth(30);
+			a.move(30);
+			a.turn(144);
+		}
+		a.hide();
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/RobotWalk
+		 * Make the robot draw a line of stars like this: http://bit.ly/RobotWalk
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
 
 	}
-
 }
---
+++ src/_10_99_bottles/Something/Something.pde
@@ -0,0 +1,9 @@
+for (int i = 97; i > 0; i--) {
+  print(i);
+  print(" bottles of beer on the wall, ");
+  print(i);
+  println(" bottles of beer.");
+  print("Take one down and pass it around, ");
+  print(i-1);
+  println(" bottles of beer on the wall.");
+}
